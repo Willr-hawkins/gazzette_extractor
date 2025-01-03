fc5fbe249161d326197f83d6c5a5b5c3feb1b3e2
William Hawkins <Hawkinswill02@gmail.com>
2025-01-03 16:35:15 +0000

    Fix wsgi configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gazzette_extractor.wsgi
+web: gunicorn gazzette_extractor.wsgi:application
--- gazzette_extractor/gazzette_extractor/settings.py
+++ gazzette_extractor/gazzette_extractor/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY' ,'')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-willrhawkin-gazzetteext-enu7srjm9lu.ws-eu117.gitpod.io']
 
