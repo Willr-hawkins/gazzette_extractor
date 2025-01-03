0912a98fd4735c51d660cd8534f6024104b8a0f9
William Hawkins <Hawkinswill02@gmail.com>
2025-01-03 15:57:25 +0000

    Added process_file view to carry out OCR processing using tesseract

--- gazzette_extractor/extractor/views.py
+++ gazzette_extractor/extractor/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from .forms import FileUploadForm
+import os
+from django.conf import settings
+
+import pytesseract
+from PIL import Image
 
 def upload_file(request):
     """ Handle upload file form submissions """
@@ -7,8 +12,18 @@ def upload_file(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            return render(request, 'extractor/success.html')
+            upload_file = from.save()
+            file_path = os.path.join(settinmgs.MEDIA_ROOT, upload_file.file.name)
+            extracted_text = process_file(file_path)
+            return render(request, 'extractor/success.html', {'text': extracted_text})
     else:
         form = FileUploadForm()
     return render(request, 'extractor/upload.html', {'form': form})
+
+def process_file(file_path):
+    """ Process the upload file using Terrseract """
+
+    image = Image.open(file_path)
+
+    text = pytesseract.image_to_string(image)
+    return text
